930b9d2ebb8ff2f9c1525628c597b684e1f26f3e
Henil Chhipani <henilchhipani@gmail.com>
2024-12-02 16:57:55 +0530

    error solving

--- src/image/image.module.ts
+++ src/image/image.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { FirebaseImageService } from 'src/firebase/firebase-image.service';
 import { ImageController } from './image.controller';
 import { ImageService } from './image.service';
 
 @Module({
     imports: [],
     controllers: [ImageController],
-    providers: [ImageService, FirebaseImageService],
+    providers: [ImageService],
 })
 export class ImageModule {}
